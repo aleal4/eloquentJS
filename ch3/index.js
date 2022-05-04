6e8acf5eb68fcb2271f927c381057c6e9e573a4e
Augusto <aleal4@gmail.com>
2022-05-03 21:55:22 -0700

    recursion

--- ch3/index.js
+++ ch3/index.js
@@ -4,3 +4,11 @@ function minimum(num1, num2){
 }
 
 // console.log(minimum(32, 23))
+
+function isEven(n){
+  if(n <= 0) return true
+  if(n === 1) return false
+  return isEven(n-2)
+}
+
+console.log(isEven(33))
