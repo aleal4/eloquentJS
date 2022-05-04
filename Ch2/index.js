ad38155f2516344cb5f7f589586a708eb5681853
Augusto <aleal4@gmail.com>
2022-05-03 19:57:19 -0700

    line length problem solution

--- Ch2/index.js
+++ Ch2/index.js
@@ -1,22 +1,24 @@
-let num = 1
-let hash = '#'
-while (num <= 7){
-  // console.log(hash)
-  hash += '#'
-  num++
-}
+// let num = 1
+// let hash = '#'
+// while (num <= 7){
+//   // console.log(hash)
+//   hash += '#'
+//   num++
+// }
+for (let line = '#'; line.length < 8; line += '#')
+console.log(line)
 
-for (let i = 1; i <= 100; i++){
-  if (i % 3 === 0 && i % 5 === 0){
-    console.log('FizzBuzz')
-  } else if (i % 5 === 0){
-    console.log('Buzz')
-  } else if (i % 3 === 0){
-    console.log('Fizz')
-  } else{
-    console.log(i)
-  }
-}
+// for (let i = 1; i <= 100; i++){
+//   if (i % 3 === 0 && i % 5 === 0){
+//     console.log('FizzBuzz')
+//   } else if (i % 5 === 0){
+//     console.log('Buzz')
+//   } else if (i % 3 === 0){
+//     console.log('Fizz')
+//   } else{
+//     console.log(i)
+//   }
+// }
 
 // for (let n= 1; n <= 100; n++){
 //   let output = ''
