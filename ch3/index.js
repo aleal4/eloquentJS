e7e5147fed3869ec4a41fd2867b7904c242e105e
Augusto <aleal4@gmail.com>
2022-05-03 21:56:32 -0700

    recursive

--- ch3/index.js
+++ ch3/index.js
@@ -5,10 +5,17 @@ function minimum(num1, num2){
 
 // console.log(minimum(32, 23))
 
-function isEven(n){
-  if(n <= 0) return true
-  if(n === 1) return false
-  return isEven(n-2)
-}
+// function isEven(n){
+//   if(n <= 0) return true
+//   if(n === 1) return false
+//   return isEven(n-2)
+// }
 
-console.log(isEven(33))
+// function isEven(n) {
+//   if (n == 0) return true;
+//   else if (n == 1) return false;
+//   else if (n < 0) return isEven(-n);
+//   else return isEven(n - 2);
+// }
+
+// console.log(isEven(-33))
