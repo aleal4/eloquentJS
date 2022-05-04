3a864d044a436fce581b0f062bcc8b4507631d70
Augusto <aleal4@gmail.com>
2022-05-03 19:52:09 -0700

    fizzbuzz

--- Ch2/index.js
+++ Ch2/index.js
@@ -1,7 +1,17 @@
 let num = 1
 let hash = '#'
 while (num <= 7){
-  console.log(hash)
+  // console.log(hash)
   hash += '#'
   num++
+}
+
+for (let i = 1; i <= 100; i++){
+  if (i % 3 === 0 && i % 5 === 0){
+    console.log('FizzBuzz')
+  } else if (i % 5 === 0){
+    console.log('Buzz')
+  } else if (i % 3 === 0){
+    console.log('Fizz')
+  }
 }
