159b86a7a3d6603e9265dd086f771249dff58eac
Augusto <aleal4@gmail.com>
2022-05-04 19:58:37 -0700

    linked list problems

--- Ch4/index.js
+++ Ch4/index.js
@@ -72,4 +72,19 @@ function listToArray(list){
   return array
 }
 
-console.log(listToArray({ value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }))
+// console.log(listToArray({ value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }))
+
+function prepend(value, list){
+  return {value, rest: list}
+}
+
+function nth(list, n){
+  if (!list) return undefined
+  else if(n == 0) return list.value;
+  else return nth(list.rest, n-1)
+}
+
+console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 2));
