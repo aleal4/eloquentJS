5b8001ff1c2ba3b4b87b8c774d6401e1be12961f
Augusto <aleal4@gmail.com>
2022-05-04 19:47:18 -0700

    got it

--- Ch4/index.js
+++ Ch4/index.js
@@ -47,18 +47,28 @@ function reverseArrayInPlace(array){
 function arrayToList(array){
   let list = null;
   for(let i = array.length - 1; i >= 0; i--){
-    list = {value: array[i], rest: list}
+    list = {
+      value: array[i], 
+      rest: list
+    }
   }
   return list
 }
 
-console.log(arrayToList([1, 2, 3]))
+// console.log(arrayToList([1, 2, 3]))
 
 function listToArray(list){
   let array = []
-  for (let node = list; node; node = node.rest){
-    array.push(node.value)
+
+  while (list !== null){
+    array.push(list.value)
+    list = list.rest
   }
+
+  // for (let node = list; node; node = node.rest){
+  //   console.log(node)
+  //   array.push(node.value)
+  // }
   return array
 }
 
