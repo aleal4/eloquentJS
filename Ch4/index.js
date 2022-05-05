6493100f4d0f04e840099436748773f50a15970f
Augusto <aleal4@gmail.com>
2022-05-04 17:15:20 -0700

    adding

--- Ch4/index.js
+++ Ch4/index.js
@@ -1,11 +1,18 @@
-function ranged (start, end){
+function range(start, end){
   let newArr = []
 
   for (let i = start; i <= end; i++){
     newArr.push(i)
   }
   return newArr
-
 }
 
-console.log(ranged(3, 7))
+function sum(array){
+  let total = 0
+  for (let i = 0; i < array.length; i++){
+    total += array[i]
+  }
+  return total
+}
+
+console.log(sum(range(1, 10)))
