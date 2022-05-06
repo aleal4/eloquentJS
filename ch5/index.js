774fdb5919088b6e70626a626c7c6ccc85f951b8
Augusto <aleal4@gmail.com>
2022-05-06 06:35:07 -0700

    repeat

--- ch5/index.js
+++ ch5/index.js
@@ -5,5 +5,26 @@ function greaterThan(n) {
 }
 
 let greaterThan10 = greaterThan(10)
-console.log(greaterThan10(11))
+// console.log(greaterThan10(11))
 
+function noisy(f){
+  return (...args) => {
+    console.log('calling with', args)
+    let result = f(...args)
+    console.log('called with', args, ", returned", result)
+    return result
+  }
+}
+
+// noisy(Math.min)(3, 2, 1)
+
+// function unless(test, then){
+//   if (!test) then()
+// }
+
+// // assumes repeat function exists
+// repeat(3, n => {
+//   unless(n % 2 == 1, () => {
+//     console.log(n, "is even")
+//   })
+// })
