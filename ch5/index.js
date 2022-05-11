772f24f734754651836204fe5c268c73b3a0af11
Augusto <aleal4@gmail.com>
2022-05-10 20:37:05 -0700

    mind bend

--- ch5/index.js
+++ ch5/index.js
@@ -31,4 +31,11 @@ function noisy(f){
 
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
-console.log(arrays.reduce((flat, current) => flat.concat(current), []))
+// console.log(arrays.reduce((flat, current) => flat.concat(current), []))
+
+function loop(start, test, update, body){
+  for (let value = start; test(value); value= update(value)){
+    body(value)
+  }
+}
+loop(3, n => n > 0, n => n - 1, console.log)
