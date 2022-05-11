f3457b11c4595307dd355a41795c64776b2e0f87
Augusto <aleal4@gmail.com>
2022-05-11 08:23:37 -0700

    dominant direction

--- ch5/index.js
+++ ch5/index.js
@@ -52,9 +52,26 @@ function every(array, test){
 //   return !array.some((element) => !test(element))
 // }
 
-console.log(every([1, 3, 5], n => n < 10));
+// console.log(every([1, 3, 5], n => n < 10));
 // → true
-console.log(every([2, 4, 16], n => n < 10));
+// console.log(every([2, 4, 16], n => n < 10));
 // → false
-console.log(every([], n => n < 10));
+// console.log(every([], n => n < 10));
 // → true
+
+function dominantDirection(text) {
+  let counted = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({name}) => name != "none");
+
+  if (counted.length == 0) return "ltr";
+
+  return counted.reduce((a, b) => a.count > b.count ? a : b).name;
+}
+
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
