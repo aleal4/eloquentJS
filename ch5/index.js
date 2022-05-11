5e5980a66a2e4adc791bfb5719fefc9cd55f29e9
Augusto <aleal4@gmail.com>
2022-05-10 20:45:31 -0700

    testing

--- ch5/index.js
+++ ch5/index.js
@@ -38,4 +38,23 @@ function loop(start, test, update, body){
     body(value)
   }
 }
-loop(3, n => n > 0, n => n - 1, console.log)
+// loop(3, n => n > 0, n => n - 1, console.log)
+
+function every(array, test){
+  for (let element of array){
+    if (!test(element)){
+      return false
+    }
+  }
+  return true
+}
+// function every(array, test) {
+//   return !array.some((element) => !test(element))
+// }
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
